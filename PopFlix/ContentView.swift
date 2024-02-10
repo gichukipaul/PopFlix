5c1ebb4953569720556db0e576455f5603b16cf3
GichukiPaul <gichukipaul043@gmail.com>
2024-02-11 00:08:59 +0300

    INIT: Initial commit

---
+++ PopFlix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopFlix
+//
+//  Created by user on 11/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PopFlix/PopFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopFlixApp.swift
+//  PopFlix
+//
+//  Created by user on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
